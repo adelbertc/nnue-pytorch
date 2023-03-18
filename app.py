7d742e339659f0456a85b5703fab50a80c108ae6
Adelbert Chang <achang@octoml.ai>
2023-03-18 11:18:16 -0700

    clean up code

--- app.py
+++ app.py
@@ -7,68 +7,73 @@ import chess
 import torch
 from octoml_profile import (accelerate, remote_profile, RemoteInferenceSession)
 
-torch._dynamo.config.suppress_errors = True
+FEATURE_SET = features.get_feature_set_from_name("HalfKAv2_hm")
 
-def read_model(nnue_path, feature_set):
+def read_model(nnue_path):
     with open(nnue_path, "rb") as f:
-        reader = serialize.NNUEReader(f, feature_set)
+        reader = serialize.NNUEReader(f, FEATURE_SET)
         return reader.model
 
-def eval_position(model, fen, feature_set, dynamite):
-    fens = [fen]
-    batch = nnue_dataset.make_sparse_batch_from_fens(feature_set, fens, [0] * len(fens), [1] * len(fens), [0] * len(fens))
+def eval_positions(model, fens, profile):
+    """
+    Evaluate the list of positions with the model.
 
-    us, them, white_indices, white_values, black_indices, black_values, outcome, score, psqt_indices, layer_stack_indices = batch.contents.get_tensors("cuda")
-    evaluation = predict(model, us, them, white_indices, white_values, black_indices, black_values, outcome, score, psqt_indices, layer_stack_indices, dynamite)
-
-    nnue_dataset.destroy_sparse_batch(batch)
-    return evaluation
-
-def predict(model, us, them, white_indices, white_values, black_indices, black_values, outcome, score, psqt_indices, layer_stack_indices, dynamite):
-    if dynamite:
+    Parameters
+    ----------
+    model
+        The PyTorch model that evaluates the position
+    fens
+        List of FEN strings to evaluate
+    """
+    if profile:
         model = accelerate(model)
 
-    result = model.forward(us, them, white_indices, white_values, black_indices, black_values, psqt_indices, layer_stack_indices) * 600.0
-    result = result.item() / 208.0
+    # Make a SparseBatch out of the FENs and extract the features
+    batch = nnue_dataset.make_sparse_batch_from_fens(FEATURE_SET, fens, [0] * len(fens), [1] * len(fens), [0] * len(fens))
+    us, them, white_indices, white_values, black_indices, black_values, outcome, score, psqt_indices, layer_stack_indices = batch.contents.get_tensors("cuda")
 
-    if them[0] > 0.5:
-       result = -result
+    # Evaluate the positions and scale them to pawn scores
+    evals = [v.item() for v in model.forward(us, them, white_indices, white_values, black_indices, black_values, psqt_indices, layer_stack_indices) * 600.0 / 208.0]
 
-    return result
+    # Set the correct evaluation depending on perspective of score
+    # (e.g. white has advantage vs. black has advantage)
+    for i in range(len(evals)):
+        if them[i] > 0.5:
+            evals[i] = -evals[i]
+        print("eval = {} for position = \"{}\"".format(evals[i], fens[i]))
+
+    nnue_dataset.destroy_sparse_batch(batch)
+    return evals
 
 def filter_fens(fens):
     def not_in_check(fen):
         board = chess.Board(fen=fen)
         return not board.is_check()
-    return [fen for fen in fens if not_in_check(fen)]
 
-def eval_positions(model, fens, feature_set, dynamite):
-    for fen in fens:
-        e = eval_position(model, fen, feature_set, dynamite)
-        print("eval = {} for position = \"{}\"".format(e, fen))
+    return list(filter(not_in_check, fens))
 
 def main():
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("--net", type=str, help="path to a .nnue net")
     parser.add_argument("--fens", type=str, help="path to file of fens")
-    parser.add_argument("--dynamite", action="store_true", help="benchmark with dynamite")
-    features.add_argparse_args(parser)
+    parser.add_argument("--profile", action="store_true", help="run in PyTorch profiling mode")
     args = parser.parse_args()
 
-    feature_set = features.get_feature_set_from_name(args.features)
-    model = read_model(args.net, feature_set)
+    model = read_model(args.net)
     model.eval()
     model.cuda()
     
     fens = filter_fens(open(args.fens).read().splitlines())
    
-    if args.dynamite:
+    if args.profile:
+        torch._dynamo.config.suppress_errors = True
+
         session = RemoteInferenceSession()
         with remote_profile(session):
             for i in range(10):
-                eval_positions(model, fens, feature_set, args.dynamite)
+                eval_positions(model, fens, args.profile)
     else:
-        eval_positions(model, fens, feature_set, args.dynamite)
+        eval_positions(model, fens, args.profile)
 
 if __name__ == "__main__":
     main()
