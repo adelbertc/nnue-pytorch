4d61f9bd950f7e3c4c6216ae1b95a92a064c204b
Adelbert Chang <achang@octoml.ai>
2023-04-18 10:32:23 -0700

    Install cuda manually to set LD_LIBRARY_PATH for cupy, add depth to predictor

--- predict.py
+++ predict.py
@@ -1,12 +1,16 @@
 import app
 import serialize
 
-from cog import BasePredictor, Input, Path
+from cog import BaseModel, BasePredictor, Input
+
+class Output(BaseModel):
+    evaluation: float
+    next_move: str
 
 
 def read_model(nnue_path):
     with open(nnue_path, "rb") as f:
-        reader = serialize.NNUEReader(f, FEATURE_SET)
+        reader = serialize.NNUEReader(f, app.FEATURE_SET)
         return reader.model
 
 
@@ -18,6 +22,10 @@ class Predictor(BasePredictor):
         self.model = model
 
     def predict(
-        self, fen: str = Input(description="FEN position to evaluate")
-    ) -> float:
-        return app.eval_positions(self.model, [fen])[0]
+        self,
+        fen: str = Input(description="FEN position to evaluate"),
+        depth: int = Input(description="Depth to search in the tree", default=1, ge=0)
+    ) -> Output:
+        evaluation, pv = app.eval_position_with_search(self.model, fen, depth)
+        next_move_string = app.get_algebraic(fen, pv)[0]
+        return Output(evaluation=evaluation, next_move=next_move_string)
